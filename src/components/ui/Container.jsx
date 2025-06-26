5752336cd46e7407d29b37a7c50b91f777ed8dad
urmishr <ramani.urmish@gmail.com>
2025-06-26 14:10:07 +0530

    Final push production

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -1,7 +1,21 @@
+import { motion } from 'motion/react';
+
 export default function Container({ children }) {
+  const initial = { x: 30, opacity: 0 };
+  const animate = {
+    x: 0,
+    opacity: 1,
+  };
+  const exit = { x: -30, opacity: 0 };
+
   return (
-    <section className="mx-4 my-5 md:m-5 px-3 py-5 flex flex-col md:px-5 space-y-5 dark:bg-dark-500 bg-light-100 shadow-light rounded-lg border border-dark-400/20 dark:border dark:border-dark-200/20">
+    <motion.div
+      initial={initial}
+      animate={animate}
+      exit={exit}
+      className="mx-4 my-5 md:m-5 px-3 py-5 flex flex-col md:px-5 space-y-5 dark:bg-dark-500 bg-light-100 shadow-light rounded-lg border border-dark-400/20 dark:border dark:border-dark-200/20"
+    >
       {children}
-    </section>
+    </motion.div>
   );
 }
--- src/components/ui/Heading.jsx
+++ src/components/ui/Heading.jsx
@@ -1,7 +1,20 @@
-export default function Heading({ children, size, type }) {
+const sizeClasses = {
+  1: 'text-2xl md:text-4xl lg:text-7xl',
+  2: 'text-lg md:text-2xl lg:text-3xl',
+  3: 'text-lg md:text-2xl lg:text-6xl',
+  // add more sizes if needed
+};
+
+export default function Heading({ children, size = 1, type }) {
+  const textSizeClass = sizeClasses[size] || sizeClasses[1];
+
   return (
     <p
-      className={`flex items-center gap-2 lg:gap-4 ${type === 'secondary' ? 'text-dark-200' : 'text-light-400 dark:text-dark-100'} ${size === 2 ? `${`text-lg md:text-2xl lg:text-3xl`}` : 'text-2xl md:text-4xl lg:text-7xl'} font-bold`}
+      className={`flex items-center gap-2 lg:gap-4 ${
+        type === 'secondary'
+          ? 'text-dark-200'
+          : 'text-light-400 dark:text-dark-100'
+      } ${textSizeClass} font-bold`}
     >
       {children}
     </p>
